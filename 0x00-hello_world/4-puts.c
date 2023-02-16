9a2aad7fed96286debc52c0a6fca1aaac704c38e
nouraldein66 <nouralaa2279@gmail.com>
2023-02-16 13:17:02 -0800

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
  * main - entry point
+ *
  * returns 0 in success operation
- * */
+ */
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
