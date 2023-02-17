dcd186f9283fdb3c00f8a240e0c36da568714ac2
nouraldein66 <nouralaa2279@gmail.com>
2023-02-17 04:02:59 -0800

    k

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int a = 97;
+
+	while (a >= 97 && a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+}
