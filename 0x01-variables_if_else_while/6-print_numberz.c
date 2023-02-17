05852dc35001385a1f6b03f7798d7e26aa430456
nouraldein66 <nouralaa2279@gmail.com>
2023-02-17 04:32:44 -0800

    l

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
