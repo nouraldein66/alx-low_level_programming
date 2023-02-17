88ffae7a29a4c935f27f704f7acb314de6335839
nouraldein66 <nouralaa2279@gmail.com>
2023-02-17 04:36:38 -0800

    k

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i <= 122 && i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar('\n');
+	return (0);
+}
