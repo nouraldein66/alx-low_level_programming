0028f386abbd7d460d3d24e16e28eb69178ead0a
nouraldein66 <nouralaa2279@gmail.com>
2023-02-21 21:48:34 -0800

    k

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
+/**
+ * print_alphabet - prints lowercase alphabet letters followed by a new line
+ *
+ * Return: always 0
+ */
 void print_alphabet(void)
 {
 	int i = 97;
+
 	while (i >= 97 && i <= 122)
 	{
 		_putchar(i);
 		i++;
 	}
 	_putchar('\n');
-}
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
