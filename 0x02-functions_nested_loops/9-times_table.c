ee9f2d7b668e0555e991d53765ccbff68e5f0ca1
nouraldein66 <nouralaa2279@gmail.com>
2023-02-22 14:16:14 -0800

    k

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = 0; j < 9; ++j)
+		{
+			if ((i * j) >= 10)
+			{
+				_putchar((i * j) / 10 + '0');
+				_putchar((i * j) % 10 + '0');
+				_putchar(',');
+			}
+			else 
+			{
+				_putchar((i * j) + '0');
+				_putchar(',');
+			}
+			if ((i * (j + 1) < 10))
+				_putchar(' ');
+			_putchar(' ');
+		}
+		if ((i * 9) >= 10)
+		{
+			_putchar((i * 9) / 10 + '0');
+			_putchar((i * 9) % 10 + '0');
+		}
+		else
+			_putchar((i * 9) + '0');
+		_putchar('\n');
+	}
+}
