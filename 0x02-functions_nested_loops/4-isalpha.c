97506a0588e1d2b81080d041e60c85b5f37e66ac
nouraldein66 <nouralaa2279@gmail.com>
2023-02-21 22:29:53 -0800

    l

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks if the input a letter or not
+ * @c: the input that will be checked
+ *
+ * Return: 1 if it's a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
