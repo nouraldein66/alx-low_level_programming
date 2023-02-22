7c7bd1e1b5504beafa1eac5784fbd903e76be0e3
nouraldein66 <nouralaa2279@gmail.com>
2023-02-21 21:43:40 -0800

    l

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
-
+void print_alphabet(void)
+{
+	int i = 97;
+	while (i >= 97 && i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
 int main(void)
 {
 	print_alphabet();
